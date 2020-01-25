6c6e9e7a29a0997c16f4c22a873b32193714fd30
Akiyo <flying.unicorn.sf@gmail.com>
2020-01-24 19:47:55 -0800

    login component added

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-
+import SignInScreen from './Login';
 class App extends Component {
 
   render(){
     return(
-      <div>let's see if this works</div>
+      <SignInScreen/>
     )
   }
 }
