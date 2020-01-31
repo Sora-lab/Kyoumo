81b9b957cb2a08ff89abe970c45cd7db185b9c3d
Akiyo <flying.unicorn.sf@gmail.com>
2020-01-31 04:26:34 -0800

    fine tuning header

--- src/components/AppBar/AppBar.tsx
+++ src/components/AppBar/AppBar.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
-interface Props{
-  layout: string;
-  leftIconOnClick: ()=>void;
+interface Props {
+	layout: string;
+	leftIconOnClick: () => void;
 }
 export function AppBar(props: Props) {
 	const today = new Date();
 	if (props.layout === 'defalut') {
 		return (
-			<header className="primary white flex space-between" style={{ padding: '8px 12px', height: '29px' }}>
-				<section>
-					<button className="no-border no-background">
+			<header
+				className="shadow primary white flex align-items-center space-between"
+				style={{ padding: '8px 12px', height: '29px', alignItems: 'center' }}
+			>
+				<section className="flex align-items-center">
+					<button className="no-border no-background" style={{marginTop: '3px'}}>
 						<i className="material-icons white">menu</i>
 					</button>
-					<span style={{ verticalAlign: 'super', paddingLeft: '1rem' }}>{today.toLocaleDateString()}</span>
+					<span style={{ paddingLeft: '1rem' }}>{today.toLocaleDateString()}</span>
 				</section>
 				<section role="toolbar ">
 					<button className="no-border no-background">
@@ -22,20 +25,21 @@ export function AppBar(props: Props) {
 			</header>
 		);
 	} else {
-    return (
-			<header className="white flex space-between primary" style={{ padding: '8px 12px', height: '29px' }}>
-				<section>
-					<button className="no-border no-background" onClick={props.leftIconOnClick}>
+		return (
+			<header
+				className="shadow primary white flex align-items-center space-between"
+				style={{ padding: '8px 12px', height: '29px', alignItems: 'center' }}
+			>
+				<section className="flex align-items-center">
+					<button className="no-border no-background" onClick={props.leftIconOnClick}  style={{marginTop: '3px'}}>
 						<i className="material-icons white">close</i>
 					</button>
-					<span style={{ verticalAlign: 'super', paddingLeft: '1rem' }}>{today.toLocaleDateString()}</span>
+					<span style={{ paddingLeft: '1rem' }}>{today.toLocaleDateString()}</span>
 				</section>
-				<section role="toolbar ">
-					<button className="white no-border no-background">
-						save
-					</button>
+				<section role="toolbar" className="white">
+					Save
 				</section>
 			</header>
-    )
-  }
+		);
+	}
 }
